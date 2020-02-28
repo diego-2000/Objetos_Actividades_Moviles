af0fc199cb7c5cbf670b87d8574a0dffbdb2c462
diego-2000 <delriosotoluisdiego@gmail.com>
2020-02-28 16:56:22 -0600

    first commit

---
+++ app/src/main/java/com/example/objetos_actividades_moviles/Mascota.java
@@ -0,0 +1,10 @@
+package com.example.objetos_actividades_moviles;
+
+public class Mascota {
+    private String nombre;
+    private String tipo;
+    private float tamaño;
+    private String color;
+    private int edad;
+
+}
